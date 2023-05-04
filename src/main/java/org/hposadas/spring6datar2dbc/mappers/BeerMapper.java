49bc752d68750ff2b15ea374deb6b7eada91e770
Humberto Posadas <posadas.humberto@outlook.com>
2023-05-04 16:27:13 +0200

    Adding MapStruct to convert between Beer and BeerDTO

---
+++ src/main/java/org/hposadas/spring6datar2dbc/mappers/BeerMapper.java
@@ -0,0 +1,12 @@
+package org.hposadas.spring6datar2dbc.mappers;
+
+import org.hposadas.spring6datar2dbc.domain.Beer;
+import org.hposadas.spring6datar2dbc.model.BeerDTO;
+import org.mapstruct.Mapper;
+
+@Mapper
+public interface BeerMapper {
+
+    Beer beerDtoToBeer(BeerDTO beerDTO);
+    BeerDTO BeerToBeerDto(Beer beer);
+}
