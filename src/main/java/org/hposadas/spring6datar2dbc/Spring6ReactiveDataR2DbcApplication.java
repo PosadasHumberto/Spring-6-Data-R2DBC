51882b43f75661ed6859c5e0d80522cd1bf98e35
Humberto Posadas <posadas.humberto@outlook.com>
2023-05-04 11:48:55 +0200

    Creating project structure

---
+++ src/main/java/org/hposadas/spring6datar2dbc/Spring6ReactiveDataR2DbcApplication.java
@@ -0,0 +1,13 @@
+package org.hposadas.spring6datar2dbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring6ReactiveDataR2DbcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring6ReactiveDataR2DbcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/hposadas/spring6datar2dbc/Spring6ReactiveDataR2DbcApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hposadas.spring6datar2dbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring6ReactiveDataR2DbcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
